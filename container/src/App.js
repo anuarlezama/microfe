e8a2dfa79ec57b933ad529c62c729659cf5482df
alezama <alezama@hushmail.me>
2023-08-29 12:07:06 -0600

    Initial commit

---
+++ container/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+export default () => {
+    return <>
+    <h4>Hi there!</h4>
+    <hr/>
+    <MarketingApp/>
+    </>;
+};
